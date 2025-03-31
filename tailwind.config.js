6ec1952bd39c4875b354ed684d4f8688442049f3
Firebrick <jbjabzin@gmail.com>
2025-03-31 11:31:05 -0700

    Prepare for Netlify deployment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          firebrick: '#B22222',
+          kirby: '#FF69B4',
+        },
+      },
+    },
+    darkMode: 'class',
+  };
+  
