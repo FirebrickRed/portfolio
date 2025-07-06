6649aaec05c90e600d15cc873764b599d5def5e0
Firebrick <jbjabzin@gmail.com>
2025-07-05 23:17:15 -0700

    Updated text for wizardoutfitter, updated modal, updated H1 tag

--- js/components/steps/WireframeStep.js
+++ js/components/steps/WireframeStep.js
@@ -1,4 +1,6 @@
 
+import { showModal } from "../Modal.js";
+
 export function renderWireframeStep(step) {
   const { description, figmaEmbed, userfeedback } = step;
   const container = document.createElement('div');
@@ -11,13 +13,31 @@ export function renderWireframeStep(step) {
     container.appendChild(descriptionEl);
   }
 
-  if (figmaEmbed) {
-    const figmaEl = document.createElement('iframe');
-    figmaEl.src = figmaEmbed;
-    figmaEl.width = '100%';
-    figmaEl.height = '450';
-    figmaEl.allowfullscreen = true;
-    container.appendChild(figmaEl);
+
+  const actionsContainer = document.createElement('div');
+  actionsContainer.className = 'delivered-actions';
+
+  if (step.previewWireFrameLink) {
+    const figmaButton = document.createElement('button');
+    figmaButton.textContent = 'Preview Wireframe';
+    figmaButton.className = 'action-button preview-button';
+    figmaButton.addEventListener('click', () => {
+      showModal(step.previewWireFrameLink);
+    });
+    actionsContainer.appendChild(figmaButton);
+  }
+
+  if (step.viewInFigmaLink) {
+    const openInNewTabButton = document.createElement('a');
+    openInNewTabButton.href = step.viewInFigmaLink;
+    openInNewTabButton.target = '_blank';
+    openInNewTabButton.textContent = 'View in Figma';
+    openInNewTabButton.className = 'action-button github-button';
+    actionsContainer.appendChild(openInNewTabButton);
+  }
+
+  if (actionsContainer.children.length > 0) {
+    container.appendChild(actionsContainer);
   }
 
   if (userfeedback) {
